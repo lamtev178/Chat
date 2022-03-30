6066ea1770b16a01166a24960c00d46f9f569aac
Lamtev Artem <lamtev2000@gmail.com>
2022-03-30 23:07:24 +0300

    added message form

--- client/chat/src/components/Topic.js
+++ client/chat/src/components/Topic.js
@@ -1,19 +1,41 @@
 import React from "react";
 import {useParams} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux'
+import {Form, Input, FormGroup, Label, Button} from 'reactstrap'
 
 function Topic() {
   const {topicID} = useParams() 
-  const tipic = useSelector(state=>state.topics[topicID-1])
-  console.log(tipic);
+  const topic = useSelector(state=>state.topics[topicID-1])
+  function handleSubmitMessage(event){
+    event.preventDefault()
+    let date = (new Date() + "").split(' ')
+    console.log(event.target[0].value);
+    console.log(date[2], date[1], date[4].slice(0,5));
+  }
   return (
     <div className="mt-5">
       <h1>
-        {tipic.title}
+        {topic.title}
       </h1><hr style={{margin:"20px 40px"}}/>
       <h4>
-        {tipic.description}
+        {topic.description}
       </h4>
+      <Form onSubmit={handleSubmitMessage} style={{paddingTop:'50px'}}>
+        <FormGroup>
+          <Label for="message">
+            Ваш комментарий
+          </Label>
+          <Input
+            style={{height:'200px'}}
+            name="message"
+            placeholder="Message"
+            type='textarea'
+          />
+        </FormGroup>
+        <Button color='primary'>
+          Отправить
+        </Button>
+      </Form>
     </div>
   );
 }
