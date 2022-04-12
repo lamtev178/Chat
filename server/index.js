a0cef57ee90bb8209e6f4123b57ba9aa31a5b9f7
Lamtev Artem <lamtev2000@gmail.com>
2022-04-13 01:55:28 +0300

    create websocket connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const http = require('http');
 const mongoose = require('mongoose')
+const WebSocket  = require('ws')
 const PORT = process.env.PORT || '8000'
 const authRouter = require('./routes/authRouter')
 const topicsRouter = require('./routes/topicsRouter')
+
 const cors = require('cors')
 const app = express()
 app.use(cors())
@@ -10,9 +13,22 @@ app.use(express.json())
 app.use('/auth',authRouter)
 app.use('/alltopics',topicsRouter)
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server ({server: server, path: '/message'});
+
+wss.on('connection', ws => {
+  ws.on('message', data => {
+    console.log('received: %s', data);
+  });
+
+  ws.on("error", e => ws.send(e));
+  ws.send('Something from server');
+});
+
+
 const start = async () =>{
   await mongoose.connect('mongodb+srv://lamtev178:qwerty123@cluster0.u8xgn.mongodb.net/Chat?retryWrites=true&w=majority')
-  app.listen(PORT, ()=> console.log('Server started on port ', PORT))
+  server.listen(PORT, ()=> console.log('Server started on port ', PORT))
 }
 
 start()
