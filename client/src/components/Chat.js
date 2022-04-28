5af9509b00438ddbb511ffe3710dc1bc66e9f892
lamtev178 <lamtev2000@gmail.com>
2022-04-28 15:53:43 +0300

    add chat info panel

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,33 +1,39 @@
 import React, {useContext, useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux'
-import {sendMess, messReaded, delUser} from '../redux/ActionCreator'
+import {sendMess, messReaded, delUser, addUsersToChat, delChat} from '../redux/ActionCreator'
 import {ThemeContext} from '../App'
 import {useParams, useNavigate} from 'react-router-dom';
 import MyInput from './UI/Input/MyInput';
 import MyButton from './UI/Button/MyButton';
+import AddUsersToChat from './AddUsersToChat'
 import Loader from './Loader'
+import Message from './Message'
 import MyModal from './UI/Modal/MyModal';
 import ModalBody from './UI/Modal/ModalBody';
 import ModalHeader from './UI/Modal/ModalHeader'
-import { BiUser, BiInfoCircle, BiX } from 'react-icons/bi';
+import { BiInfoCircle, BiX } from 'react-icons/bi';
 
-function Chat({handleSendMess}){
+function Chat({handleSendMess, handleChecked, checkedState}){
   const dispatch = useDispatch()
   const redirect = useNavigate()
   const {chatID} = useParams()
   const [toggle, setToggle] = useState(false)
+  const [active, setActive] = useState("users")
   const [message, setMessage] = useState('')
   const chat = useSelector(state => state.chats.find((ch) => ch.chat === chatID)) || []
-  const login = useSelector(state => state.isAuth.user.login) || []
+  const myUser = useSelector(state => state.isAuth.user) || []
+  const subs = myUser.subscriptions.filter(user => chat.users.indexOf(user) === -1 )
+  const chatCreator = chat.users[chat.users.length-1]===myUser.login ? true : false;
+console.log(chat);
+  const {theme} = useContext(ThemeContext)
   const [isLoading, setIsLoading] = useState(
-    new Array(chat.users.length).fill(false)
+    new Array(subs.length).fill(false)
 )
   useEffect(()=>{
     setIsLoading(
       new Array(chat.users.length).fill(false)
     )
   },[chat])
-  const {theme} = useContext(ThemeContext)
   async function hanleSubmit(){
     dispatch(sendMess(message, chatID, handleSendMess))
     setMessage('')
@@ -43,6 +49,48 @@ function Chat({handleSendMess}){
     )
     setIsLoading(finishLoading)
   }
+  let content
+  if(active==="users"){
+    content = (
+      <div className="users-list">
+        {chat.users.map( (user, index) => {
+          return(
+            <div className="justifyBetween" key={user}>
+              <h3>
+                {user}
+              </h3>
+              { (user === myUser.login || chatCreator) ?
+                isLoading[index] ? <Loader style={{margin:"0", padding:"0 15px 0 0"}}/> :
+                <BiX
+                  onClick={() => delUserFromChat(user, index)}
+                  style={{marginRight:"20px", cursor:"pointer", opacity:"0.7"}}
+                /> : null
+              }
+            </div>
+          )
+        })}
+      </div>
+    )
+  } else if(active === "addUsers"){
+    content = (
+      <>
+        <AddUsersToChat allSubs={subs} handleChecked={handleChecked} checkedState={checkedState}/>
+        { subs.length === 0 ? null :
+          <MyButton onClick ={() => dispatch(addUsersToChat(chatID, checkedState, subs, setToggle))}>
+            Добавить
+          </MyButton>
+        }
+      </>
+    )
+  } else if(active === "deleteChat")
+    content = ( chatCreator ? (
+      <>
+        <h3> Вы действительно хотите удалить чат ? </h3>
+        <MyButton onClick={()=> dispatch(delChat(chatID, redirect))}>Удалить чат</MyButton>
+      </>
+    ):(
+      <h3>Удалить чат может только создатель</h3>
+    ))
   useEffect(async()=>{
     document.documentElement.scrollIntoView(false)
     dispatch(messReaded(chatID))
@@ -51,25 +99,13 @@ function Chat({handleSendMess}){
     <>
       <div className={!theme ? "chat-ligth" : "chat-dark"}>
         <div className={"justifyBetween " + (!theme ? "chat-header-ligth" : "chat-header-dark")}>
-          <h2>{chat.chatName.length > 0 ? chat.chatName : chat.users[Number(!chat.users.indexOf(login))]}</h2>
+          <h2>{chat.chatName.length > 0 ? chat.chatName : chat.users[Number(!chat.users.indexOf(myUser.login))]}</h2>
           <BiInfoCircle style={{width:"28px", height:"28px", cursor:"pointer"}} onClick={()=>setToggle(true)}/>
         </div>
         {chat.length === 0 ? null : chat.messages.map(mess => {
           return(
-            <div key={mess._id}>
-              <div className={theme ? "box-dark" : "box-light"} style={{padding:'10px'}} >
-                <p style={{wordBreak: "break-all", fontSize:'20px', marginBottom:"0"}} className={(mess.isReaded ? "" :
-                  (theme ? "message-not-readed-dark" : "message-not-readed-light"))}>{mess.message}</p>
-              </div>
-              <div className="comments-info" style={{fontSize:'16px'}} >
-                <p>
-                  <BiUser />{mess.author}
-                </p>
-                <p>{mess.date}</p>
-              </div>
-            </div>
-          )})
-        }
+            <Message mess={mess} key={mess._id}/>
+        )})}
         <div className={"justifyBetween " + (!theme ?  "sendMessForm" : "sendMessForm-dark")}>
           <MyInput onChange={e => setMessage(e.target.value)} value={message}/>
           <MyButton onClick={hanleSubmit} style={{marginLeft:"15px"}}>Отправить</MyButton>
@@ -77,27 +113,20 @@ function Chat({handleSendMess}){
       </div>
       <MyModal toggle={toggle}>
       <ModalHeader onClick={() => setToggle(false)}>
-        Участники
+        <div className="modalHeader justifyAround">
+          <h3 className={active === "users" ? "active" : ""} onClick={()=> setActive("users")}>Участники</h3>
+          <h3 className={active === "addUsers" ? "active" : ""} onClick={()=> setActive("addUsers")}>Добавить участников</h3>
+          <h3
+            style={{marginRight:"50px"}}
+            className={active === "deleteChat" ? "active" : ""}
+            onClick={()=> setActive("deleteChat")}
+          >
+            Удалить чат
+          </h3>
+        </div>
       </ModalHeader>
       <ModalBody>
-        <div className="users-list">
-          {chat.users.map( (user, index) => {
-            return(
-              <div className="justifyBetween" key={user}>
-                <h3>
-                  {user}
-                </h3>
-                {
-                  isLoading[index] ? <Loader style={{margin:"0", padding:"0 15px 0 0"}}/> :
-                  <BiX
-                    onClick={() => delUserFromChat(user, index)}
-                    style={{marginRight:"20px", cursor:"pointer"}}
-                  />
-                }
-              </div>
-            )
-          })}
-        </div>
+        {content}
       </ModalBody>
       </MyModal>
     </>
